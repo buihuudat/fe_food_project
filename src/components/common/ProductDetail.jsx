c2259890f93f7251346b8572ad69ddc35d2ff35e
Bui Huu Dat <dat54261001@gmail.com>
2023-06-01 18:40:06 +0700

    update

--- src/components/common/ProductDetail.jsx
+++ src/components/common/ProductDetail.jsx
@@ -3,8 +3,17 @@ import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
 import { useDispatch, useSelector } from "react-redux";
-import { setProductDetails } from "../../redux/reducers/productReducer";
+import {
+  setProductDetails,
+  setProducts,
+} from "../../redux/reducers/productReducer";
 import currentFormat from "../../handler/currentFormat";
+import StarIcon from "@mui/icons-material/Star";
+import { IconButton } from "@mui/material";
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+import PlaylistAddCheckIcon from "@mui/icons-material/PlaylistAddCheck";
+import { setCart } from "../../redux/reducers/cartReducer";
+import { useLocation } from "react-router-dom";
 
 const style = {
   position: "absolute",
@@ -25,8 +34,54 @@ const style = {
 export default function ProductDetail() {
   const { status: open, data } = useSelector((state) => state.products.details);
   const dispatch = useDispatch();
+  const addCart = useSelector((state) => state.cart.data);
+  const dataProduct = useSelector((state) => state.products.data);
+  const { pathname } = useLocation();
+
   const handleClose = () => {
     dispatch(setProductDetails({ status: false, data: {} }));
+  };
+
+  const handleAdd = (e) => {
+    let isExist = false;
+    let cartItems = [...addCart];
+    let productItems = [...dataProduct];
+
+    cartItems.map((v, i) => {
+      if (e._id === v._id) {
+        isExist = true;
+        cartItems[i] = {
+          ...v,
+          countCartUser: v.countCartUser + 1,
+          count: v.count - 1,
+        };
+      }
+    });
+
+    productItems.map((v, i) => {
+      if (e._id === v._id) {
+        productItems[i] = {
+          ...v,
+          count: v.count - 1,
+          countCartUser: v.countCartUser + 1,
+        };
+        dispatch(setProducts([...productItems]));
+      }
+    });
+
+    isExist && dispatch(setCart([...cartItems]));
+
+    !isExist &&
+      dispatch(
+        setCart([
+          ...cartItems,
+          {
+            ...e,
+            countCartUser: e.countCartUser + 1,
+            count: e.count - 1,
+          },
+        ])
+      );
   };
 
   return (
@@ -59,6 +114,23 @@ export default function ProductDetail() {
             >
               {currentFormat(data.price)}
             </Typography>
+            <Box>
+              <StarIcon color="warning" />
+              <StarIcon color="warning" />
+              <StarIcon color="warning" />
+              <StarIcon color="warning" />
+              <StarIcon color="warning" />
+            </Box>
+            {pathname.split("/")[1] !== "admin" &&
+              (data.countCartUser > 0 ? (
+                <IconButton disabled>
+                  <PlaylistAddCheckIcon color="success" />
+                </IconButton>
+              ) : (
+                <IconButton onClick={() => handleAdd(data)}>
+                  <AddShoppingCartIcon color="primary" />
+                </IconButton>
+              ))}
           </Box>
         </Box>
       </Modal>
